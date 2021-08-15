2051c839f7aed7c970b25d92066a5fcdbe70b6f3
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-08-15 09:35:01 +0530

    add quesn-num remove ask-every

--- index.js
+++ index.js
@@ -17,27 +17,27 @@ if(user_name.length<1){
 }
 console.log(chalk.yellowBright.bold("\n Hey " +user_name.toUpperCase()+",\n\n This is a quiz based on STARWARS franchise\n"))
 
-console.log(chalk.greenBright("Here are the rules:\n1. Every correct answer adds one point to the score \n2. There is no penalty for wrong answers.\n3. There are 2 levels, answer all the questions on levelZero to reach Level1\n\nNOTE--This quiz is a two level play where if the user clears L1 then his name is replaced with current winner"))
+console.log(chalk.greenBright("Here are the rules:\n1. Every correct answer adds one point to the score \n2. There is no penalty for wrong answers.\n3. There are 2 levels with 5 questions in each level. Answer atleast 4 correct on levelZero to reach Level1\n\nNOTE--This quiz is a two level play where if the user clears L1 then his name is replaced with current winner"))
 // ===========levels using arrays and objects================
 let levelZero=[
-  {q:"\nStawars franchise was created by?--",a:"george lucas"},
-  {q:"In which year did the first Starwars film release?--",a:"1977"},
-  {q:"In which Starwars flim did Emilia Clarke star--",a:"Solo"},
-  {q:"Who was the first Skywalker?--",a:"Shmi skywalker"},
-  {q:"What was the last Starwars flim in theatres?--",a:"Rise of skywalker"}]
+  {q:"\n1. Stawars franchise was created by?--",a:"george lucas"},
+  {q:"2. In which year did the first Starwars film release?--",a:"1977"},
+  {q:"3. In which Starwars flim did Emilia Clarke star--",a:"Solo"},
+  {q:"4. Who was the first Skywalker?--",a:"Shmi skywalker"},
+  {q:"5. What was the last Starwars film in theatres?--",a:"Rise of skywalker"}]
 
 let levelOne=[
-  {q:"\nName of the latest starwars series?--",a:"Mandalorian"},
-  {q:"How many triologies does Starwars have?--",a:"Three"},
-  {q:"What's the best Starwars vehicle?--",a:"Millennium Falcon"},
-  {q:"Next Starwars film?--",a:"Rogue Squadron"},
-  {q:"upcoming starwars series?--",a:"Andor"}]
+  {q:"\n6. Name of the latest starwars series?--",a:"Mandalorian"},
+  {q:"7. Starwars has 3 triologies--YES or NO?",a:"YES"},
+  {q:"8. What's the best Starwars vehicle?--",a:"Millennium Falcon"},
+  {q:"9. Next Starwars film?--",a:"Rogue Squadron"},
+  {q:"10. Upcoming starwars series?--",a:"Andor"}]
 // ============output==================
 var user_answer;
 function game(question,answer){
   user_answer=readlineSync.question(question)
   while(user_answer.length<1){
-    console.log(chalk.red("Please enter an answer"))
+    console.log(chalk.red("Please enter an answer."))
     user_answer=readlineSync.question(question)
   }
   
@@ -56,22 +56,15 @@ function game(question,answer){
 // ===================input=================
 function on_level(level){
   for(var i=0;i<level.length;i++){
-    let enter=readlineSync.question(chalk.blue.bold("Press Y to continue,any other key to quit the Quiz anytime--"))
-    if(enter.toUpperCase()!='Y'){
-      console.log("Thanks for checking out the game!!")
-      break
-    }
-    else{
       let holder=level[i];
       game(holder.q,holder.a)
-    }
   }
 }
 // =============for final results===============
 function result(level){
   console.log(chalk.blue("========\nTotal questions-"+(score +wrong)))
   console.log(chalk.blue("Ansered correct-"+score))
-  console.log(chalk.blue('Ansered Wrong-'+ wrong+"\n======="))
+  console.log(chalk.blue('Ansered Wrong-'+ wrong+"\n========"))
   console.log("Highest score user--"+ high[0].user+"\nscore--"+high[0].score)
   console.log(chalk.yellowBright.bold("\nCONGRATULATIONS"))
   console.log("\nTo know more about STARWARS, visit")
